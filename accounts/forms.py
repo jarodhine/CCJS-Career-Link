672160e51ed5db185e75484f8cab51f465562e45
jarodhine <jarod.hine@my.jcu.edu.au>
2019-09-19 10:11:10 +1000

    Updated Admin model

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from .models import CustomUser
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-class UserRegisterForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField()
     first_name = forms.CharField()
     last_name = forms.CharField()
@@ -11,3 +11,9 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ['email', 'first_name', 'last_name', 'password1', 'password2']
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ['email']
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
@@ -18,11 +18,11 @@ def accountView(request):
 
 def registerView(request):
     if request.method == "POST":
-        form = UserRegisterForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login_url')
     else:
-        form = UserRegisterForm()
+        form = CustomUserCreationForm()
 
     return render(request, 'registration/register.html', {'form':form})
